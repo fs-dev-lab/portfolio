d41dbde70438240bc61b8408390ee2186c11ff39
fs-dev-lab <coltonro820@gmail.com>
2025-10-14 01:34:35 -0700

    Remove bottom line in navbar component

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -17,7 +17,7 @@ function Navbar() {
   ];
 
   return (
-    <nav className="fixed top-0 left-0 w-full bg-black/40 backdrop-blur-md z-[9999] border-b border-b-[#16f2b3]">
+    <nav className="fixed top-0 left-0 w-full bg-black/40 backdrop-blur-md z-[9999]">
       <div className="flex items-center justify-between px-6 md:px-24 py-4 h-[70px]">
         <Link
           href="/"
